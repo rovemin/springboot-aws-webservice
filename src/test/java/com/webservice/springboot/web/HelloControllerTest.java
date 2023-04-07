db57c694f5d98415ea0a2aa598952e9c17d5deb0
Minseo Cho <min1130@ewhain.net>
2023-04-07 23:19:10 +0900

    [5장] 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기

--- src/test/java/com/webservice/springboot/web/HelloControllerTest.java
+++ src/test/java/com/webservice/springboot/web/HelloControllerTest.java
@@ -1,24 +1,31 @@
 package com.webservice.springboot.web;
 
+import com.webservice.springboot.config.auth.SecurityConfig;
 import com.webservice.springboot.web.HelloController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.is;
 
-@WebMvcTest(controllers = HelloController.class)        // Web(Spring MVC)에 집중할 수 있는 어노테이션
-                                                        // 내부에 @ExtendWith(SpringExtension.class) 적용되어 있음
-    // 선언 시 @Controller, @ControllerAdvice 등은 사용 가능하지만, @Service, @Component, @Repository 등은 사용 불가
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired  // 스프링이 관리하는 빈(Bean)을 주입받음
     private MockMvc mvc;    // 웹 API 테스트 시 사용. 스프링 MVC 테스트의 시작점
                             // 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트 가능
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -31,6 +38,7 @@ public class HelloControllerTest {
                                                         // Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
